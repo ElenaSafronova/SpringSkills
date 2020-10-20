f6c3861ec79732716f3dc752442af27f996ba86c
Elena Safronova <elenasafronova1987@gmail.com>
2020-10-20 20:01:08 +0300

    initial commit

---
+++ src/test/java/ru/safronova/SpringSkills/SpringSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safronova.SpringSkills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
